27a74fd9e70e23deef34c50fbfdde14fb5611feb
sanyagargg <133576817+sanyagargg@users.noreply.github.com>
2025-02-25 19:56:29 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Navbar from "./mainPageComponents/Navbar"; // Corrected path
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-poppins font-bold text">Habit Tracker Website</h1>
+      <Navbar />
+      <h1 className="font-poppins font-bold text-xl text-center mt-10">Habit Tracker Website</h1>
     </div>
   );
 }
