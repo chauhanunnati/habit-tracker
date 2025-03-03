13055919a2ec1c2f87c85186b72fc23857604266
Kasturi Sinha <sinha.kasturi.05@gmail.com>
2025-03-03 18:25:27 +0530

    connected clerk to mongoDB using endpoint

---
+++ app/Models/UsersSchema.ts
@@ -0,0 +1,14 @@
+import { EmailAddress } from "@clerk/nextjs/server";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        clerkUserId: { type: String, unique: true, required: true },
+        EmailAddress: { type: String, required: true },   
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
