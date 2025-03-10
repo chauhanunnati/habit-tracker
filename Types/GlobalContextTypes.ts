fc75665f50c8a2e54743b7adb4b23ef44b8355b1
radhikag0 <gargradhika2004@gmail.com>
2025-03-10 22:21:04 +0530

    making the website responsive

--- Types/GlobalContextTypes.ts
+++ Types/GlobalContextTypes.ts
@@ -1,9 +1,14 @@
+import { Dispatch, SetStateAction } from "react";
 import { menuItemType } from "./MenuItemTypes";
-import {Dispatch, SetStateAction } from "react";
+
 
 export type GlobalContextType = {
-    menuItemsObject: {
-        menuItems: menuItemType[];
-        setMenuItems: Dispatch<SetStateAction<menuItemType[]>>;
-    };
+  menuItemsObject: {
+    menuItems: menuItemType[];
+    setMenuItems: Dispatch<SetStateAction<menuItemType[]>>;
+  };
+  openSideBarObject: {
+    openSideBar: boolean;
+    setOpenSideBar: Dispatch<SetStateAction<boolean>>; // Ensure this is present
+  };
 };
--- app/Pages/AllHabits/components/AllHabitsTopBar.tsx
+++ app/Pages/AllHabits/components/AllHabitsTopBar.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import AllHabitsSearchBar from "./AllHabtitsSearchBar";
 import DarkMode from "./DarkMode";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { UserButton, UserProfile } from "@clerk/nextjs";
 
 function AllHabitsTopBar() {
+  const userButtonAppearance = {
+    elements: {
+      userButtonAvatarBox: "w-10 h-10", 
+      userButtonPopoverActionButton: "text-red-600", 
+    },
+  };
+
   return (
-    <div className="bg-white p-5 rounded-md flex justify-between w-full items-center">
-      <div className="flex flex-col">
-        <span className="text-xl">
-          <span className="font-bold">Hi There!</span>
-          <span className="font-light">, Ali</span>
-        </span>
-        <span className="font-light text-[12px] text-gray-400">
-          Welcome back!
-        </span>
+    <div className="bg-white p-5 rounded-md flex justify-between">
+      <div className="flex gap-4">
+        <div className="max-lg:flex hidden">
+          <UserButton appearance={userButtonAppearance} />
+        </div>
+
+        <div className="flex flex-col max-md:hidden">
+          <span className="text-xl">
+            <span className="font-bold">Hi There</span>,
+            <span className="font-light"> Alix</span>
+          </span>
+          <span className="font-light text-[12px] text-gray-400">
+            Welcome back!
+          </span>
+        </div>
       </div>
 
-      <div className="w-[90%] flex gap-3 justify-between">
+      <div className="w-[50%] max-md:w-[80%] flex gap-3 justify-between">
         <AllHabitsSearchBar />
         <DarkMode />
+        <FontAwesomeIcon
+          className="m-2 max-xl:flex hidden mt-[13px] cursor-pointer"
+          icon={faBars}
+        />
       </div>
     </div>
   );
