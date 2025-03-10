f6c83eed11b62e7e0b41005ddd46964130636f97
Kasturi Sinha <sinha.kasturi.05@gmail.com>
2025-03-10 20:18:17 +0530

    added statistics component to the right side bar

--- app/Pages/AllHabits/components/AllHabitsRightSideBar.tsx
+++ app/Pages/AllHabits/components/AllHabitsRightSideBar.tsx
@@ -4,11 +4,13 @@ import React from "react";
 //import { defaultColour } from "@/colors";
 //import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import UserProfile from "./RightSideBar/UserProfile";
+import MainStatistics from "./RightSideBar/MainStatistics";
 
 function AllHabitsRightSideBar() {
     return (
         <div className="w-[30%] flex flex-col items-center-center bg-white">
             <UserProfile />
+            <MainStatistics />
         </div>
     );
 }
---
+++ app/Pages/AllHabits/components/RightSideBar/MainStatistics.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
+import { defaultColour } from "@/colors";
+
+function MainStatistics() {
+    const statisticsInfo = [
+        { id: 1, num: 10, subTitle: "Best streaks" },
+        { id: 2, num: 12, subTitle: "Perfect days" },
+    ];
+
+    return (
+            <div 
+                className="flex mx-4 flex-col gap-6 justify-center items-center mt-20
+                    bg-slate-50 rounded-xl p-5 pt-7"
+            >
+                <span className="font-bold text-xl cursor-pointer hover:text-customRed">
+                    Statistics
+                </span>
+
+            {/* The circular progress bar */}
+            <div className="relative pt-3">
+                <CircularProgressBar progress={80} />
+                <div className="flex flex-col justify-center items-center absolute top-[54%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <span className="font-bold text-xl text-customRed">80%</span>
+                    <span className="text-[11px]">{ `Today's Progress` }</span>
+                </div>
+            </div>
+
+            {/* Streaks and perfect days */}
+            <div className="my-4 w-full flex justify-center gap-6 flex-wrap items-center w-full">
+                {statisticsInfo.map((singleItem, singleItemIndex) => (
+                    <div className="flex items-center gap-3" key={singleItemIndex}>
+                        <div className="flex h-2 w-2 bg-customRed rounded-full"></div>
+                        <span className="flex flex-col font-bold">{singleItem.num}</span>
+                        <span className="text-gray-500">{singleItem.subTitle}</span>
+                    </div>
+                ))}
+            </div>
+            </div>
+    );
+}
+
+interface CircularProgressBarProps {
+   progress: number; 
+}
+
+const CircularProgressBar: React.FC<CircularProgressBarProps> = ({  
+    progress,  
+}) => {  
+    const data = [  
+        { name: "Completed", value: progress },  
+        { name: "Remaining", value: 100 - progress },  
+    ];  
+
+    const COLORS = [defaultColour.default, "#edf2f4"];  
+
+    return (  
+        <PieChart  
+            width={200}  
+            height={200}  
+            // className="bg-red-300"  
+            margin={{ top: -20, right: 0, bottom: 40, left: 0 }}  
+        >  
+            <Pie  
+                data={data}  
+                cx={100}  
+                cy={100}  
+                startAngle={180}  
+                endAngle={-180}  
+                innerRadius={66}  
+                outerRadius={progress === 100 ? 80 : 78}  
+                fill="#8844d8"  
+                paddingAngle={0}  
+                dataKey="value"  
+            >  
+                {data.map((entry, index) => (  
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />  
+            ))}  
+</Pie>  
+        </PieChart>  
+    );  
+};  
+
+export default MainStatistics;
