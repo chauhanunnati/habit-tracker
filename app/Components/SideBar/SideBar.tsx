f05636fb9dbf2d01dbead559c6c1df69e7a9f649
Unnati <unnatichauhan2004@gmail.com>
2025-03-11 13:40:55 +0530

    Updated SideBar.tsx for DarkMode

--- app/Components/SideBar/SideBar.tsx
+++ app/Components/SideBar/SideBar.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import LogoAnName from "../LogoAnName";
 import { useGlobalContextProvider } from "@/Types/contextApi";
-import { DarkModeItem } from "./DarkModeTypes";
 import MenuSelection from "./MenuSelection";
 import LogoutSection from "./LogoutSection";
+import { darkModeColor, defaultColor } from "@/colors";
 
 function Sidebar() {
-  const { openSideBarObject } = useGlobalContextProvider();
-  const { openSideBar, setOpenSideBar } = openSideBarObject; // Ensure correct destructuring
-  const { isDakrMode } = darkModeObject;
+  const { openSideBarObject, darkModeObject } = useGlobalContextProvider();
+  const { openSideBar, setOpenSideBar } = openSideBarObject;
+  const { isDarkMode } = darkModeObject;
+  const sideBarRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    function handleOutsideClicked(event: MouseEvent) {
+      if (!sideBarRef.current?.contains(event.target as Node)) {
+        setOpenSideBar(false);
+      }
+    }
+    document.addEventListener("click", handleOutsideClicked);
+    return () => {
+      document.removeEventListener("click", handleOutsideClicked);
+    };
+  }, [openSideBar, setOpenSideBar]);
 
   return (
     <div
+      ref={sideBarRef}
+      style={{
+        color: isDarkMode ? darkModeColor.textColor : defaultColor.textColor,
+        backgroundColor: isDarkMode
+          ? darkModeColor.background
+          : defaultColor.background,
+      }}
       className={`${
         !openSideBar ? "max-xl:hidden" : "fixed shadow-lg"
       } flex-grow z-50 p-10 flex-col bg-white min-h-screen`}
--- colors.ts
+++ colors.ts
@@ -1,11 +1,13 @@
-export const defaultColour = {
+export const defaultColor = {
     default: "#d90429",
+    textColor: "#000000",
+    background: "#FFFFFF",
     50: "rgba(217, 4, 41, 0.5)",
     100: "rgba(217, 4, 41, 0.05)",
 
 };
 
 export const darkModeColor = {
-    background: "003049",
-    textColor: "FFFFFF",
+    background: "#003049",
+    textColor: "#FFFFFF",
 }
